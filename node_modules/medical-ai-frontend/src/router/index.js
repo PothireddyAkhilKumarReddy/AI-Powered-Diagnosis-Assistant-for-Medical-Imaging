774e48a55970b79592aa49705b4fdbf7d3752720
PothireddyAkhilKumarReddy <2300031067@kluniversity.in>
2026-02-21 23:50:52 +0530

    fix: restore missing SignupPage import that caused blank screen

--- node_modules/medical-ai-frontend/src/router/index.js
+++ node_modules/medical-ai-frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
 import LoginPage from '../pages/LoginPage.vue'
+import SignupPage from '../pages/SignupPage.vue'
 import DashboardPage from '../pages/DashboardPage.vue'
 import PricingPage from '../pages/PricingPage.vue'
 import AboutPage from '../pages/AboutPage.vue'
